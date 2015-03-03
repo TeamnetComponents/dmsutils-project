b819f77108b24204edc35d832e081d7103741060
Razvan <razvan.ionescu@teamnet.ro>
2015-03-03 15:17:23 +0200

    jdbc eip working test

---
+++ dmsutils-tests/src/test/java/ro/tn/components/dmsutils/eip/JdbcIntegrationTest.java
@@ -0,0 +1,26 @@
+package ro.tn.components.dmsutils.eip;
+
+import integration.config.DmsConfig;
+import integration.config.IntegrationConfig;
+import integration.config.JdbcConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Razvan.Ionescu on 3/3/2015.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JdbcConfig.class,  DmsConfig.class, IntegrationConfig.class})
+public class JdbcIntegrationTest {
+    @Test
+    public void testIntegration() throws Exception {
+        while (true) {
+            /*Message message = queueChannel.receive(1000);
+            if (message != null) {
+                System.out.println(message);
+            }*/
+        }
+    }
+}
