a99d80af59e0acb0397f984ae9b6e90979d35ff9
Hanna <hanna.botar@teamnet.ro>
2015-03-03 20:25:02 +0200

    process first message

--- dmsutils-eip/src/main/java/integration/service/JdbcService.java
+++ dmsutils-eip/src/main/java/integration/service/JdbcService.java
@@ -2,6 +2,7 @@ package integration.service;
 
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Service;
 import ro.croco.integration.dms.toolkit.StoreServiceMessage;
 import ro.croco.integration.dms.toolkit.db.StoreServiceMessageDb;
@@ -14,6 +15,11 @@ import java.util.List;
 public class JdbcService {
 
     public  Message<List<StoreServiceMessage>> inspectResponse(final Message<List<StoreServiceMessage>> message){
+        System.out.println("Response = " + message.getPayload());
+        return message;
+    }
+
+    public  Message<StoreServiceMessage> inspectSingleResponse(final Message<StoreServiceMessage> message){
         System.out.println("Response = " + message.getPayload());
         return message;
     }
@@ -33,5 +39,27 @@ public class JdbcService {
             }
         };
         return responseMessage;
+    }
+
+    public  Message<StoreServiceMessage> processSingleRequest(final Message<List<StoreServiceMessage>> message){
+        System.out.println("Request = " + message.getPayload());
+
+        Message<List<StoreServiceMessage>> responseMessage = new Message<List<StoreServiceMessage>>() {
+            @Override
+            public MessageHeaders getHeaders(){
+                return null;
+            }
+
+            @Override
+            public List<StoreServiceMessage> getPayload() {
+                return message.getPayload();
+            }
+        };
+        return new GenericMessage<StoreServiceMessage>(message.getPayload().get(0));
+    }
+
+    public  Message<List<StoreServiceMessage>> afterUpdate(final Message<List<StoreServiceMessage>> message){
+        System.out.println("After update = " + message.getPayload());
+        return message;
     }
 }
